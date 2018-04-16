6b7a89436ff5c072d9821e1bb74aabfdd947ba0a
Fraser McKay <frasermckay97@gmail.com>
2018-04-17 00:25:17 +0100

    added in welcome page

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ require 'sinatra/contrib/all' if development?
 also_reload("./models")
 require_relative './models/rps_game.rb'
 
+
 get '/' do
   erb(:home)
 end
 
+get '/welcome_page' do
+  erb (:welcome_page)
+end
 
 get '/game/:player1/:player2' do
   @result = Game.rock_paper_scissors(params[:player1], params[:player2])
-return "#{@result}"
+erb (:result)
 end
