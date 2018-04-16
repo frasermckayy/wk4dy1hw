3d68c5039d1fa5e714aa1f8592f09fcd33495e14
Fraser McKay <frasermckay97@gmail.com>
2018-04-16 20:56:30 +0100

    mvp

--- app.rb
+++ app.rb
@@ -6,3 +6,9 @@ require_relative './models/rps_game.rb'
 get '/' do
   erb(:home)
 end
+
+
+get '/game/:player1/:player2' do
+  @result = Game.rock_paper_scissors(params[:player1], params[:player2])
+return "#{@result}"
+end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,15 +1,16 @@
 class Game
 
   def self.rock_paper_scissors(player1, player2)
-    if player1 = "rock" && player2 = "scissors"
-      return "player1 wins!"
-    elsif player1 = "scissors" && player2 = "paper"
-      return "player 1 wins!"
-    elsif player1 = "paper" && player2 = "rock"
-      return "player1 wins!"
-    elsif player1 == player2
+    if player1 == player2
       return "It's a draw!"
-    else "player2 wins!"
+    elsif player1 == "rock" && player2 == "scissors"
+      return "player1 wins!"
+    elsif player1 == "scissors" && player2 == "paper"
+      return "player 1 wins!"
+    elsif player1 == "paper" && player2 == "rock"
+      return "player1 wins!"
+    else
+      return "player2 wins!"
     end
   end
 end
